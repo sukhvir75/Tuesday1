7ebbfd5839bb09ca5bd67b8d90b4163b3552df8d
sukhvir75 <140761481+sukhvir75@users.noreply.github.com>
2023-08-03 01:13:10 -0400

    Update DemoTuesday.java

--- src/Demo1/DemoTuesday.java
+++ src/Demo1/DemoTuesday.java
@@ -4,6 +4,9 @@ public class DemoTuesday {
 	public static void main(String args []) {
 		System.out.println("How are you");
 	}
+	public static void add(int a,int b){
+		System.out.println(a+b);
+	}
 
 
 }
